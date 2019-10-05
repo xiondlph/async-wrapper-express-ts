b2f8a1f67b5ba5b42e5b77b6b18f12cbebbd07da
Shukhrat <shukhrat@ismax.ru>
2019-10-05 18:45:53 +0300

    Fix type definition

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import {Request, Response, NextFunction, RequestHandler} from "express";
 
 declare function asyncWrapper(
-    fn: (req: Request, res: Response, next: NextFunction) => Promise<Response | undefined>
+    fn: (req: Request, res: Response, next: NextFunction) => Promise<Response | void>
 ): RequestHandler;
 
 export = asyncWrapper;
